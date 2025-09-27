92c2d6da04f1093e4410c05491b615fc372253cd
Akniyeet <akniyet.maksatova@mail.ru>
2025-09-27 23:40:19 +0500

    Create StudentClient.java

---
+++ StudentClient.java
@@ -0,0 +1,19 @@
+package adapterdemo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class StudentClient {
+
+    public List<Student> getStudentList() {
+        List<Student> students = new ArrayList<>();
+
+        CollegeStudent collegeStudent = new CollegeStudent("a", "b", "c");
+        SchoolStudent schoolStudent = new SchoolStudent("x", "y", "z");
+
+        students.add(collegeStudent);
+        students.add(new SchoolStudentAdapter(schoolStudent)); // Adapter for SchoolStudent
+
+        return students;
+    }
+}
